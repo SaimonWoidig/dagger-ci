a79ce13085b8e882563aa898010b090bcbc6d605
Šimon Woidig <woidig.simon@gmail.com>
2023-11-27 22:55:31 +0100

    added simple api backend

---
+++ backend/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+type Task struct {
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var InMemoryTask Task = Task{
+	Title:     "Learn Dagger",
+	Completed: false,
+}
+
+const ListenAddr string = ":8080"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			slog.Info("request", "path", "/", "method", http.MethodGet)
+
+			data, err := json.Marshal(InMemoryTask)
+			if err != nil {
+				slog.Error("error in marshal", "error", err.Error())
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			if _, err = w.Write(data); err != nil {
+				slog.Error("error in writing response", "error", err.Error())
+			}
+		case http.MethodPut:
+			slog.Info("request", "path", "/", "method", http.MethodPut)
+
+			bodyBytes, err := io.ReadAll(r.Body)
+			if err != nil {
+				slog.Error("error in reading body", "error", err.Error())
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			contentTypeHeader := r.Header.Get("Content-Type")
+			var contentType string
+			if contentTypeHeader != "" {
+				contentType = contentTypeHeader
+			} else {
+				contentType = http.DetectContentType(bodyBytes)
+			}
+			if contentType != "application/json" {
+				slog.Warn("unsupported content type", "contentType", contentType)
+				w.WriteHeader(http.StatusUnsupportedMediaType)
+				return
+			}
+
+			if len(bodyBytes) <= 0 {
+				slog.Warn("empty body")
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			var task Task
+			if err := json.Unmarshal(bodyBytes, &task); err != nil {
+				slog.Warn("error in unmarshal", "error", err.Error())
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			if task.Title == "" {
+				slog.Warn("empty title in put")
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			slog.Info("overwriting task in memory", "newTitle", task.Title, "newCompleted", task.Completed)
+			InMemoryTask = task
+			w.WriteHeader(http.StatusAccepted)
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
+
+	s := &http.Server{
+		Addr:              ListenAddr,
+		Handler:           mux,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+	}
+
+	slog.Info("starting server", "addr", ListenAddr)
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			slog.Error("error while serving", "error", err.Error())
+		}
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	slog.Info("shutting down server")
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		slog.Error("error while shutting down server", "error", err.Error())
+	}
+}
